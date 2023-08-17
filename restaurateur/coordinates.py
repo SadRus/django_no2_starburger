7a795a0457fd9c549a9dec1efe0c23e624faf2c1
SadRus <rystixx@yandex.ru>
2023-08-17 21:48:32 +0300

    Refactoring

--- restaurateur/coordinates.py
+++ restaurateur/coordinates.py
@@ -1,29 +1,28 @@
 import requests
 
-from requests.exceptions import RequestException
-
 from geocoder.models import Place
 
 
 def fetch_coordinates(apikey, address):
-    place, place_is_created = Place.objects.get_or_create(address=address)
-    if place_is_created:
-        base_url = 'https://geocode-maps.yandex.ru/1.x'
-        try:
+    try:
+        place, place_is_created = Place.objects.get_or_create(address=address)
+        if place_is_created:
+            base_url = 'https://geocode-maps.yandex.ru/1.x'
             response = requests.get(base_url, params={
                 'geocode': address,
                 'apikey': apikey,
                 'format': 'json',
             })
             response.raise_for_status()
-        except RequestException:
-            return None
 
-        found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-        most_relevant = found_places[0]
-        lon, lat = most_relevant['GeoObject']['Point']['pos'].split(' ')
-        place.longitude = lon
-        place.latitude = lat
-        place.save()
+            found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+            most_relevant = found_places[0]
+            lon, lat = most_relevant['GeoObject']['Point']['pos'].split(' ')
+            place.longitude = lon
+            place.latitude = lat
+            place.save()
+            return place.latitude, place.longitude
         return place.latitude, place.longitude
-    return place.latitude, place.longitude
+    except Exception as err:
+        print(err)
+        return None, None
