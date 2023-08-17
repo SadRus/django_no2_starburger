dd95d934f9765a07427620cb927cbdab4e403112
SadRus <rystixx@yandex.ru>
2023-08-17 21:47:06 +0300

    Set default DEBUG to False

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -14,7 +14,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 YA_GEOCODER_KEY = env('YA_GEOCODER_KEY')
 
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env.bool('DEBUG', True)
+DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
