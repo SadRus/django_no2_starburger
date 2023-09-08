013b7bb2246b34b885ca8476cae3b5f4fd480514
SadRus <rystixx@yandex.ru>
2023-09-08 13:53:50 +0300

    switch to PostgreSQL DB

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -102,7 +102,7 @@ MEDIA_URL = env('MEDIA_URL', '/media/')
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, env('DATABASE', 'db.sqlite3')))
+        default=env('DB_URL', f'sqlite:////{os.path.join(BASE_DIR, "db.sqlite3")}')
     )
 }
 
